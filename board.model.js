2df5d82f1619c01e4a7e5cba2c7180cd6c0abb29
GeneHFang <gene.li@nyu.edu>
2020-03-26 23:35:33 -0400

    added all board attributes to Schema after testing endpoints on Postman

--- board.model.js
+++ board.model.js
@@ -2,8 +2,35 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-let Board = new Schema({
+let Board = new Schema({ 
+    //STRING REPRESENTATION OF BOARD
     board:{ type: String },
+    
+    //GRID SIZE
+    gridNum:{ type: Number },
+    
+    //TOP PLAYER ATTRIBUTES
+    topColor: { type: String },
+    topShape: { type: String },
+    
+    //BOTTOM PLAYER ATTRIBUTES
+    bottomColor: { type: String },
+    bottomShape: { type: String },
+    
+    //TURN LOGIC
+    clicked:  { type: Boolean },
+    turn: { type: String },
+
+    //SAVED HILIGHTED PIECE OBJECT
+    hilightedPiece:  {
+        row:{
+            type: Number},
+        col:{
+            type: Number},
+        topOrBottom:{
+            type: String
+        }
+    },
 });
 
 module.exports = mongoose.model('Board', Board);
--- server.js
+++ server.js
@@ -31,10 +31,18 @@ boardRoute.route('/').get((request, response) => {
 
 //SET BOARD
 boardRoute.route('/set').post(async (request, response) => {
+    //UPDATE BOARD IF EXISTS
     let exists = await Board.find();
-    //UPDATE EXISTING BOARD
     if (exists[0]){
         exists[0].board = request.body.board;
+        exists[0].gridNum = request.body.gridNum;
+        exists[0].topColor = request.body.topColor;
+        exists[0].topShape = request.body.topShape;
+        exists[0].bottomColor = request.body.bottomColor;
+        exists[0].bottomShape = request.body.bottomShape;
+        exists[0].clicked = request.body.clicked;
+        exists[0].turn = request.body.turn;
+        exists[0].hilightedPiece = request.body.hilightedPiece;
         exists[0].save()
             .then(board => {
                 response.status(200).json({'board': 'board saved successfully'});
@@ -55,6 +63,18 @@ boardRoute.route('/set').post(async (request, response) => {
             response.status(400).send('board not set');
         });
     }
+});
+
+//RESET BOARD (DELETE FROM DB)
+boardRoute.route('/reset').delete(async (request, response) => {
+    let board = await Board.find();
+    board[0].remove()
+        .then( board => {
+            response.status(200).json({"message":"removed"})
+        })
+        .catch( error => {
+            response.status(400).send("error, board not removed")
+        })
 });
 
 //BASE ROUTE
